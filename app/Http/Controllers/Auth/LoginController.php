1a40416643bda3c4f21c2c6169be3ebeaf2dab03
mohamedellithy <develomohamed22@gmail.com>
2023-10-24 13:28:47 +0300

    djddc

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,6 +40,6 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-        return view('theme_2.auth.login');
+        return view(config('app.theme').'.auth.login');
     }
 }
