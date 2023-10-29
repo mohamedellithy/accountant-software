ab99492d23967ca2a723643d2fa4997117ff8ef2
mohamedellithy <develomohamed22@gmail.com>
2023-10-29 11:00:56 +0200

    djssdsسddssssيsxsسيddssdwssddس

--- resources/views/Theme_2/auth/login.blade.php
+++ resources/views/Theme_2/auth/login.blade.php
@@ -14,7 +14,7 @@
                         <input type="hidden" name="email" value="admin@admin.com" />
                         <div class="mb-3 form-password-toggle">
                             <div class="d-flex justify-content-between">
-                                <label class="form-label" for="password">{{ __('Password') }}</label>
+                                <label class="form-label" for="password">{{ __('كلمة المرور') }}</label>
                             </div>
                             <div class="input-group input-group-merge">
                                 <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" />
